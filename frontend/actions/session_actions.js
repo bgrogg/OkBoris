c600273599d0a586ff88c1867d61f05245f00996
bgrogg <borisgrogg@Boriss-MacBook-Air.local>
2017-01-11 13:15:18 -0800

    Create sessionAPIUtil, reducers, and store

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,44 @@
+import { signup, login, logout } from '../util/session_api_util';
+
+export const LOGIN = "LOGIN";
+export const LOGOUT = "LOGOUT";
+export const SIGNUP = "SIGNUP";
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const RECEIVE_LOGOUT_SUCCESS = "RECEIVE_LOGOUT_SUCCESS";
+export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
+
+export const requestSignup = user => dispatch => {
+  return signup(user).then(
+    currentUser => dispatch(receiveCurrentUser(currentUser))
+  ).fail(
+    error => dispatch(receiveErrors(error.responseJSON))
+  );
+};
+
+export const requestLogin = user => dispatch => {
+  return login(user).then(
+    currentUser => dispatch(receiveCurrentUser(currentUser))
+  ).fail(
+    error => dispatch(receiveErrors(error.responseJSON))
+  );
+};
+
+export const requestLogout = () => dispatch => {
+  return logout().then(
+    () => dispatch(receiveLogoutSuccess())
+  );
+};
+
+export const receiveCurrentUser = currentUser => ({
+  type: RECEIVE_CURRENT_USER,
+  currentUser
+});
+
+export const receiveLogoutSuccess = () => ({
+  type: RECEIVE_LOGOUT_SUCCESS
+});
+
+export const receiveErrors = errors => ({
+  type: RECEIVE_ERRORS,
+  errors
+});
--- frontend/okboris.jsx
+++ frontend/okboris.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store.js';
 
 document.addEventListener('DOMContentLoaded', () => {
+    window.store = configureStore();
     const root = document.getElementById('root');
     ReactDOM.render(<h1>Welcome to OkBoris!</h1>, root);
 });
