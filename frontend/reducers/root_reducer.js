b1d62140cdaea8d45cfbfe99fcae71d09657eeaf
bgrogg <borisgrogg@Boriss-MacBook-Air.local>
2017-01-13 11:20:38 -0800

    Fix error in root reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,13 +1,10 @@
 import { combineReducers } from 'redux';
 import SessionReducer from './session_reducer';
 import ModalReducer from './modal_reducer';
-import UserReducer from './user_reducer';
-
 
 const RootReducer = combineReducers({
   session: SessionReducer,
-  modal: ModalReducer,
-  user: UserReducer
+  modal: ModalReducer
 });
 
 export default RootReducer;
